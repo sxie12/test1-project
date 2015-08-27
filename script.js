f0e2c534a70d02cdec53e4541a4088469669676c
Shunping Xie <sxie12@illinois.edu>
2015-08-27 01:35:07 -0500

    changed a lot of things

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 // This is the JavaScript file
 
 var load = function() {
-    alert("Page loaded");
+    // below is for testing only
+    // alert("Page loaded");
 }
+
+var hide_first_column = function() {
+    $('#column_one_button').click(function() {
+        $('div.column_one_list').toggle();
+    });
+}
+
+/* does not work for resizing
+var max_column_height = function() {
+    var col1h = $('.column_one').height();
+    var col2h = $('.column_two').height();
+    var col3h = $('.column_three').height();
+    var maxHeight = Math.max(col1h, col2h, col3h);
+    var height = "height:" + maxHeight + "px";
+    document.getElementById('column_one').setAttribute("style", height);
+    document.getElementById('column_two').setAttribute("style", height);
+    document.getElementById('column_three').setAttribute("style", height);
+}
+*/
 
 $(document).ready(load);
 
 $(document).ready(hide_first_column);
 
-var hide_first_column = function() {
-    $('#column_one_button').click(function() {
-        alert("Clicked");
-        $('div.column_one_list').toggle();
-    });
-}
+// $(document).ready(max_column_height);
 
-/*
-$(document).on("click", ".column-one-button", function() {
-    alert("Clicked");
-    $('div.column-one-list').toggle();
-});
-*/
-
-/*
-function hide-column-one() {
-    $('div.column-one-list').toggle();
-} 
-*/
